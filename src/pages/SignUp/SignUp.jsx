979222fe4e0c83b5fbaf5bbdc8fe4fcfcf661110
Mursheda-2003 <murshedaakterpinky@gmail.com>
2025-01-20 23:30:00 +0600

    new

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,18 +1,13 @@
 
-import { useForm } from "react-hook-form"
-import login from "../../assets/others/authentication2.png"
-import "../login/Login"
 import { Link } from "react-router-dom";
+import login from "../../assets/others/authentication2.png"
+import { useForm } from "react-hook-form"
 import { CiFacebook } from "react-icons/ci";
 import { FaGoogle } from "react-icons/fa";
-import { RiGithubFill } from "react-icons/ri";
-import logo from "../../assets/logo.png"
-
-
-
-
-
-const SignUp = () => {
+import { VscGithub } from "react-icons/vsc";
+import logo from "../../assets/logo.png";
+import "../login/Login"
+const SignUP = () => {
   const {
     register,
     handleSubmit,
@@ -20,66 +15,76 @@ const SignUp = () => {
     formState: { errors },
   } = useForm()
 
+  console.log(errors)
   const onSubmit = (data) => console.log(data)
   return (
     <div className="bg-img">
-    <Link to="/">
-          <img className="w-16 ml-24" src={logo} alt="navlogo" /></Link>
-    <div className="flex flex-row-reverse bg-img h-screen">
-      
-      <img src={login} alt="" />
-      <form onSubmit={handleSubmit(onSubmit)}>
+       <Link to="/">
+          <img className="w-16 ml-24" src={logo} alt="navlogo" />
+          </Link>
+      <div className="flex flex-row-reverse h-screen justify-center gap-40 py-5">
+ <div>
+ <img src={login} alt="" />
+ </div>
+   <div className="w-96">
+    <h1 className="text-center text-xl mt-10 font-bold">SignUp</h1>
+   <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
-      <h1 className="text-center text-xl font-bold flex justify-center ">SignUp</h1>
-      <div>
-         <label htmlFor="" className="text-xl">Name:</label><br />
-         <input className="focus:outline-none px-2 py-2 w-96" placeholder="Name" {...register("name", { required: true, minLength: 1, maxLength: 20 })} />
-         {
-          errors.name && <p className="text-rose-400">Name min lenth 1 character and max lenth 20 charecter</p>
-         }
+      <div> 
+        <label htmlFor="" className="text-xl">Name</label>
+        <br />
+        <input className="mb-5 focus:outline-none px-2 py-2 w-96" placeholder="Name" {...register("name",  { required: true, minLength: 1,  maxLength: 20 })} />
+    {
+        errors.name && <p className="text-rose-400">Name min length 1 character and   Name max length 20 character</p>
+    }
       </div>
       <div>
-         <label htmlFor="" className="text-xl">Email:</label><br />
-         <input className="focus:outline-none px-2 py-2 w-96" placeholder="Email" {...register("email")} />
+        <label htmlFor="" className="text-xl">Email</label>
+        <br />
+        <input className="mb-5 focus:outline-none px-2 py-2 w-96" placeholder="Email" {...register("email")} />
       </div>
       <div>
-         <label htmlFor="">Password:</label><br />
-         <input className="focus:outline-none px-2 py-2 w-96" placeholder="Password"  {...register("password",{ required: true,   minLength: {
-          value: 1,
-        message:"Your password must be six character"},
-        maxLength: { value: 20,
-          message:"Your password must be six character"} 
-          , pattern:{
-            value:/^[a-zA-Z0-9!@#$%^&*]{6,16}$/,
-            message:"It must contain at least one uppercase,one lowercase and one nemericcharecter and one special charecter "
-          } })} />
-         {
-          errors.name && <p className="text-rose-400">{errors.password.message}</p>
-         }
+        <label htmlFor="" className="text-xl">Password</label>
+        <br />
+        <input className="focus:outline-none px-2 py-2 w-96" placeholder="password" {...register("password", { required: true,
+        minLength: {
+          value: 6,
+          message: "Your password must be six character"
+        }, 
+        maxLength:{
+          value: 20,
+          message: "Your password must be 20 character long."
+        },
+        pattern: {
+          value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+          message: "It must contain at least one uppercase, one lowercase and one numeric character one scpecial character."
+        }
+          })} />
+        {
+            errors.password && <p className="text-rose-400">{errors.password.message}</p>
+        }
       </div>
-      <input className="btn mt-5 bg-orange-400 hover:bg-orange-500 text-white w-96" type="Submit" value={"SignUp"}/>
-      <p className="mt-5 text-orange-500">Already registered? 
-      <Link className="text-orange-500 font-bold " to="/login" href="">Go to log in</Link>
-    </p>
-    <p className="text-center text-xl mt-5">Or sign up with</p>
-    <div className=" flex gap-5 mt-5 justify-center">
+
+      <input className="btn mt-5 bg-orange-400 hover:bg-orange-400 text-white w-96" type="submit" value="SignUp" /> 
+    </form>
+    <p className="mt-5">Already Registerd <Link className="text-orange-400" to="/login">
+    Go to in Login</Link></p>
+    <p className="text-center mt-5 text-xl">or signin with</p>
+    <div className="flex gap-5 mt-5 justify-center">
       <button>
       <CiFacebook className="text-3xl"/>
       </button>
       <button>
-      <FaGoogle className="text-3xl" />
+      <FaGoogle className="text-2xl"/>
       </button>
       <button>
-      <RiGithubFill className="text-3xl" />
+      <VscGithub  className="text-2xl"/>
       </button>
     </div>
-    </form>
-    
-    
+   </div>
     </div>
     </div>
-    
   );
 };
 
-export default SignUp;
+export default SignUP;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,11 @@
-import { useForm } from "react-hook-form"
-import login from "../../assets/others/authentication2.png"
-import "./Login.css"
 import { Link } from "react-router-dom";
+import login from "../../assets/others/authentication2.png"
+import { useForm } from "react-hook-form"
 import { CiFacebook } from "react-icons/ci";
 import { FaGoogle } from "react-icons/fa";
-import { RiGithubFill } from "react-icons/ri";
-import logo from "../../assets/logo.png"
-
-
-
-
+import { VscGithub } from "react-icons/vsc";
+import logo from "../../assets/logo.png";
+import "./Login.css"
 const Login = () => {
   const {
     register,
@@ -20,44 +16,47 @@ const Login = () => {
 
   const onSubmit = (data) => console.log(data)
   return (
-    <div className="bg-img">
-      <Link to="/">
-      <img className="w-16 ml-24" src={logo} alt="navlogo" /></Link>
-    <div className="flex  h-screen">
-      <img src={login} alt="" />
-      <form onSubmit={handleSubmit(onSubmit)}>
+  <div className="bg-img">
+    <Link to="/">
+    <img className="w-16 ml-24" src={logo} alt="navlogo" />
+    </Link>
+    <div className="flex  h-screen justify-center gap-40 py-5">
+ <div>
+ <img src={login} alt="" />
+ </div>
+   <div>
+    <h1 className="text-center text-xl mt-10 font-bold">Login</h1>
+   <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
-      <h1 className="text-center text-xl font-bold flex justify-center ">Login</h1>
       <div>
-         <label htmlFor="" className="text-xl">Email:</label><br />
-         <input className="focus:outline-none px-2 py-2 w-96" placeholder="Email" {...register("email")} />
+        <label htmlFor="" className="text-xl">Email</label>
+        <br />
+        <input className="mb-5 focus:outline-none px-2 py-2 w-96" placeholder="Email" {...register("email")} />
       </div>
       <div>
-         <label htmlFor="">Password:</label><br />
-         <input className="focus:outline-none px-2 py-2 w-96" required placeholder="Password"  {...register("password")} />
+        <label htmlFor="" className="text-xl">Password</label>
+        <br />
+        <input className="focus:outline-none px-2 py-2 w-96" placeholder="password" {...register("pasword")} />
       </div>
-      <input className="btn mt-5 bg-orange-400 hover:bg-orange-500 text-white w-96" type="Submit" value={"SignIn"}/>
-      <p className="mt-5 text-orange-500">New Here?
-      <Link className="text-orange-500 font-bold " to="/signUp" href="">Creat a New Account</Link>
-    </p>
-    <p className="text-center text-xl mt-5">or signin with</p>
-    <div className=" flex gap-5 mt-5 justify-center">
+
+      <input className="btn mt-5 bg-orange-400 hover:bg-orange-400 text-white w-96" type="submit" value="SignIn" />
+    </form>
+    <p className="mt-5">New Here? <Link className="text-orange-400" to="/signup">Create a New Account</Link></p>
+    <p className="text-center mt-5 text-xl">or signin with</p>
+    <div className="flex gap-5 mt-5 justify-center">
       <button>
       <CiFacebook className="text-3xl"/>
       </button>
       <button>
-      <FaGoogle className="text-3xl" />
+      <FaGoogle className="text-2xl"/>
       </button>
       <button>
-      <RiGithubFill className="text-3xl" />
+      <VscGithub  className="text-2xl"/>
       </button>
     </div>
-    </form>
-    
-    
+   </div>
     </div>
-    </div>
-    
+  </div>
   );
 };
 
